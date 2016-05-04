f86d97f1e94d1a121618077399a56d405d7458a8
Ziva Skof <zs3732@student.uni-lj.si>
2016-05-04 22:02:50 +0000

    preveri še enkrat

--- eracun.js
+++ eracun.js
@@ -133,7 +133,11 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      if (napaka){
+        callback(null);
+      }else{
+        callback(vrstice);
+      }
     })
 }
 
@@ -142,13 +146,39 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+      if(napaka){
+        callback(null);
+      }else{
+      callback(vrstice);
+      }
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
+  var form = new formidable.IncomingForm();
+  form.parse(zahteva, function (napaka1, polja, datoteke) {
+    var racunId = polja.seznamRacunov;
+    strankaIzRacuna(polja.seznamRacunov, function(stranka){
+      pesmiIzRacuna(polja.seznamRacunov, function(pesmi){
+        odgovor.setHeader('content-type', 'text/xml');
+        odgovor.render('eslog', {
+           vizualiziraj: true,
+           postavkeRacuna: pesmi,
+           FirstName: stranka.FirstName,
+           LastName: stranka.LastName,
+           Company: stranka.Company,
+           Address: stranka.Address,
+           City: stranka.City,
+           Country: stranka.Country,
+           PostalCode: stranka.PostalCode,
+           Phone: stranka.Phone,
+           Fax: stranka.Fax,
+           Email: stranka.Email,
+        })
+      })
+    })
+  })
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
